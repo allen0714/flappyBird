64eb40bcff0ae5aaaf8f5c300e01ed8204c39a3d
朱锦金 <466366297@qq.com>
2018-11-01 17:38:40 +0800

    状态枚举

--- js/DataBus.js
+++ js/DataBus.js
@@ -1,4 +1,12 @@
 let instance = null;
+
+const [
+  MAIN,
+  INTRO,
+  PLAY,
+  DEAD,
+  OVER
+] = [0,1,2,3,4]; //阶段枚举值
 /**
  * 全局状态管理器
  */
@@ -13,7 +21,27 @@ export default class DataBus {
     this.isGameOver = false; //游戏是否结束
     this.score = 0; //分数
     this.aniId = 0; // 动画句柄
+    this.phase = MAIN; //游戏状态， 当前所处阶段
+  }
 
+  goToMain() {
+    this.phase = MAIN;
+  }
+
+  goToIntro() {
+    this.phase = INTRO;
+  }
+
+  goToPlay() {
+    this.phase = PLAY;
+  }
+
+  goToDead() {
+    this.phase = DEAD;
+  }
+
+  goToOver() {
+    this.phase =OVER;
   }
 
 }
