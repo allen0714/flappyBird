c6a32de9c4793d32405b4ad7464f00729ab99915
朱锦金 <466366297@qq.com>
2018-11-19 15:57:19 +0800

    解决组件提前渲染

--- js/App.js
+++ js/App.js
@@ -8,6 +8,22 @@ import Play from './routes/Play';
 
 const ctx = canvas.getContext('2d');
 
+const getRoute = (phase) => {
+  switch(phase) {
+    case 'MENU': 
+      return new Menu();
+    case 'INTRO':
+    console.info('xxxx');
+      return new Intro();
+    case 'RANK':
+      return new Rank();
+    case 'PLAY':
+      return new Play();
+    default:
+      throw new Error('unknown router of phase:' + phase);
+  }
+} 
+
 
 export default class App {
   constructor() {
@@ -20,16 +36,10 @@ export default class App {
 
   render() {
     const { phase } = this.dataBus;
-    const component =
-      {
-        MENU: new Menu(),
-        INTRO: new Intro(),
-        RANK: new Rank(),
-        PLAY: new Play(),
-      }[phase];
-    if (component.render) {
+    const route = getRoute(phase);
+    if (route.render) {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      component.render();
+      route.render();
     } else {
       throw new Error(`组件${phase}没有定义render方法`);
     }
--- js/routes/Intro.js
+++ js/routes/Intro.js
@@ -2,13 +2,14 @@ import Bird from '../runtime/Bird';
 import DataBus from '../DataBus';
 import Tutorial from '../runtime/Introduction';
 import BackGround from '../runtime/Background';
+import Route from '../base/Route.js';
 
 let instance;
 const dataBus = new DataBus();
 // const bird = new Bird();
-export default class Intro {
-
+export default class Intro extends Route{
   constructor() {
+    super();
     if (instance) {
       return instance;
     }
@@ -17,6 +18,7 @@ export default class Intro {
     this.backGround = new BackGround();
     this.tutorial = new Tutorial();
   }
+  onTouchScreen
   render() {
     this.backGround.render();
     this.tutorial.render();
